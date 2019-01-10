13ab4a8f7cad9ea51cf0220e751b13cb637e15be
Andrew Mellen <andrew_mellen@icloud.com>
2019-01-10 00:05:13 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 # drone-helm-gh-pages-deploy
 [![Build Status](https://ci.andrewmellen.org/api/badges/mellena1/drone-helm-gh-pages-deploy/status.svg)](https://ci.andrewmellen.org/mellena1/drone-helm-gh-pages-deploy)
 
-A plugin for drone.io to deploy charts to Github Pages
+A plugin for drone.io to deploy charts to Github Pages. This plugin will search the repo for charts, package them all, and then deploy them to the branch hosting Github Pages.
+
+## Build
+To build the docker image:
+```bash
+docker build -t mellena1/drone-helm-gh-pages-deploy .
+```
+
+## Usage
+Execute from the working directory (should be repo with the charts):
+```bash
+docker run --rm \
+    -e DRONE_REMOTE_URL=git@github.com:mellena1/helm-charts.git \
+    -e PLUGIN_SSH_KEY=$(cat ~/.ssh/id_rsa | base64) \
+    -v $(pwd):$(pwd) \
+    -w $(pwd) \
+    mellena1/drone-helm-gh-pages-deploy
+```
+
+## Variables
+| .drone.yml option | env var | description | default |
+|-------------------|---------|-------------|---------|
+| ssh_key | PLUGIN_SSH_KEY | base64 encoded private ssh key to pull the repo and push to it **(required)** | - |
+| charts_folder | PLUGIN_CHARTS_FOLDER | folder to search for charts from | charts |
+| github_pages_branch | PLUGIN_GITHUB_PAGES_BRANCH | branch that github pages is running off of | gh-pages |
+| overwrite_existing | PLUGIN_OVERWRITE_EXISTING | overwrite existing charts if they already exist | false |
+| git_email | PLUGIN_GIT_EMAIL | git email to make the commit with | drone@drone.io |
+| git_name | PLUGIN_GIT_NAME | git name to make the commit with | drone |
+| commit_message | PLUGIN_COMMIT_MESSAGE | commit message to make commit with | [drone.io] Added new charts. |
+| push | PLUGIN_PUSH | whether to push or not | false |
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,14 @@
 #!/usr/local/bin/bash
 
 # Vars:
-# PLUGIN_CHARTS_FOLDER
-# PLUGIN_GITHUB_PAGES_BRANCH
-# PLUGIN_OVERWRITE_EXISTING
-# PLUGIN_GIT_EMAIL
-# PLUGIN_GIT_NAME
-# PLUGIN_COMMIT_MESSAGE
-# PLUGIN_PUSH
-# PLUGIN_BRANCH_DIR
-# PLUGIN_SSH_KEY
+# PLUGIN_CHARTS_FOLDER - folder to search for charts from
+# PLUGIN_GITHUB_PAGES_BRANCH - branch that github pages is running off of
+# PLUGIN_OVERWRITE_EXISTING - overwrite existing charts if they already exist
+# PLUGIN_GIT_EMAIL - git email to make the commit with
+# PLUGIN_GIT_NAME - git name to make the commit with
+# PLUGIN_COMMIT_MESSAGE - commit message to make commit with
+# PLUGIN_PUSH - whether to push or not
+# PLUGIN_SSH_KEY - base64 encoded private ssh key to pull the repo and push to it (required)
 
 # DEFAULT VARIABLE VALUES
 PLUGIN_CHARTS_FOLDER=${PLUGIN_CHARTS_FOLDER:-"charts"}
@@ -19,7 +18,6 @@ PLUGIN_GIT_EMAIL=${PLUGIN_GIT_EMAIL:-"drone@drone.io"}
 PLUGIN_GIT_NAME=${PLUGIN_GIT_NAME:-"drone"}
 PLUGIN_COMMIT_MESSAGE=${PLUGIN_COMMIT_MESSAGE:-"[drone.io] Added new charts."}
 PLUGIN_PUSH=${PLUGIN_PUSH:-"true"}
-PLUGIN_BRANCH_DIR=${PLUGIN_BRANCH_DIR:-"droneghpages"}
 if [ -z "$PLUGIN_SSH_KEY" ]; then
   echo "-- ERROR: Must set ssh_key!"
   exit 1
@@ -27,7 +25,7 @@ fi
 
 function convertGithubToSSH() {
   # Returns $git_repo_url
-  if $(echo $DRONE_REMOTE_URL | grep https://github.com); then
+  if [ $(echo "$DRONE_REMOTE_URL" | grep "https://github.com") ]; then
     USER=`echo $DRONE_REMOTE_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\1#p'`
     if [ -z "$USER" ]; then
       echo "-- ERROR: Could not identify User."
@@ -41,7 +39,7 @@ function convertGithubToSSH() {
     fi
   
     git_repo_url="git@github.com:$USER/$REPO.git"
-  elif $(echo $DRONE_REMOTE_URL | grep "git@github.com"); then
+  elif [ $(echo "$DRONE_REMOTE_URL" | grep 'git@github.com') ]; then
     git_repo_url="$DRONE_REMOTE_URL"
   else
     echo "-- ERROR: Invalid github url."
@@ -54,9 +52,10 @@ echo $PLUGIN_SSH_KEY | base64 -d > ~/.ssh/id_rsa
 chmod 0400 ~/.ssh/id_rsa
 
 chartsdir="drone_builtcharts"
+ghpages_branch="droneghpages"
 
 # Find and package all charts
-mkdir $chartsdir
+mkdir -p $chartsdir
 for f in $(find $PLUGIN_CHARTS_FOLDER -name Chart.yaml); do
   chart=$(dirname $f)
   packagedchart=$(helm package $chart | grep -o "[^ ]*.tgz")
@@ -65,30 +64,41 @@ done
 
 # Clone the github pages branch
 convertGithubToSSH
-ssh-agent bash -c "ssh-add ~/.ssh/id_rsa; git clone -b \"$PLUGIN_GITHUB_PAGES_BRANCH\" --single-branch \"$git_repo_url\" \"$PLUGIN_BRANCH_DIR\""
+ssh-agent bash -c "ssh-add ~/.ssh/id_rsa; git clone -b \"$PLUGIN_GITHUB_PAGES_BRANCH\" --single-branch \"$git_repo_url\" \"$ghpages_branch\"" &> /dev/null
 
+charts_added=false
 # Move (or overwrite) all new charts onto the branch
 for f in $(ls $chartsdir); do
-  if [ -e "$PLUGIN_BRANCH_DIR/$f" ]; then
+  if [ -e "$ghpages_branch/$f" ]; then
     if [ "$PLUGIN_OVERWRITE_EXISTING" = true ]; then
-      rm "$PLUGIN_BRANCH_DIR/$f"
-      mv "$chartsdir/$f" "$PLUGIN_BRANCH_DIR/"
+      rm "$ghpages_branch/$f"
+      mv "$chartsdir/$f" "$ghpages_branch/"
+      echo "Updated: $f"
+      charts_added=true
     else
       rm "$chartsdir/$f"
     fi
   else
-    mv "$chartsdir/$f" "$PLUGIN_BRANCH_DIR/"
+    mv "$chartsdir/$f" "$ghpages_branch/"
+    echo "Added: $f"
+    charts_added=true
   fi
 done
 
-# Cleanup
+# Cleanup packaged charts dir
 rm -r "$chartsdir"
 
 
-cd "$PLUGIN_BRANCH_DIR"
-# Remake index.yaml
-rm -f index.yaml
-helm repo index .
+cd "$ghpages_branch"
+# Remake index.yaml if there are new charts
+if [ charts_added = true ]; then
+  rm -f index.yaml
+  helm repo index .
+else
+  echo "Nothing new to update."
+  cd .. && rm -rf "$ghpages_branch"
+  exit 0
+fi
 
 # Push to repo
 if [ "$PLUGIN_PUSH" = true ]; then
@@ -98,3 +108,7 @@ if [ "$PLUGIN_PUSH" = true ]; then
   git commit -m "$PLUGIN_COMMIT_MESSAGE"
   ssh-agent bash -c "ssh-add ~/.ssh/id_rsa; git push origin \"$PLUGIN_GITHUB_PAGES_BRANCH\""
 fi
+
+# Cleanup
+cd .. && rm -rf "$ghpages_branch"
+exit 0
